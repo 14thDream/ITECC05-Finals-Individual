ee4be2482f2d3502826bd84c00e6cf68a0b6a2ad
14thDream <dndelara@proton.me>
2024-12-09 17:01:39 +0800

    Create Login Form with MySql and VB.NET

--- DBProject_1/Form1.vb
+++ DBProject_1/Form1.vb
@@ -20,4 +20,40 @@ Public Class Form1
 
     End Sub
 
+
+    Private Sub Button_Login_Click(sender As Object, e As EventArgs) Handles Button_Login.Click
+        Dim SqlConnection = New MySqlConnection With {
+            .ConnectionString = "server=localhost;userid=root;password=;database=dbproject1"
+        }
+
+        Try
+            SqlConnection.Open()
+
+            Dim Query As String = $"SELECT * FROM edata WHERE username = '{TextBox_Username.Text}' AND password = '{TextBox_Password.Text}';"
+            Dim Command = New MySqlCommand(Query, SqlConnection)
+            Dim Reader = Command.ExecuteReader()
+
+            Dim Count As Integer = 0
+            While Reader.Read
+                Count += 1
+            End While
+
+            If Count = 1 Then
+                MessageBox.Show("Username and password are correct")
+            ElseIf Count > 1 Then
+                MessageBox.Show("Username and password are duplicates")
+            Else
+                MessageBox.Show("Username and password are not correct")
+            End If
+
+            SqlConnection.Close()
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            SqlConnection.Dispose()
+        End Try
+
+    End Sub
+
+
 End Class
