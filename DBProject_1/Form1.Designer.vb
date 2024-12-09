419bc8862df5b82ebc18a229cebbcdb0eb060e17
14thDream <dndelara@proton.me>
2024-12-09 16:17:50 +0800

    Getting Started and Mysql database Connection

--- DBProject_1/Form1.Designer.vb
+++ DBProject_1/Form1.Designer.vb
@@ -22,10 +22,32 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.AutoSize = True
+        Button1.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(282, 205)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(235, 39)
+        Button1.TabIndex = 0
+        Button1.Text = "Check Connection"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
 
 End Class
--- DBProject_1/Form1.vb
+++ DBProject_1/Form1.vb
@@ -1,3 +1,23 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim SqlConnection = New MySqlConnection With {
+            .ConnectionString = "server=localhost;userid=root;password=;database=dbproject1"
+        }
+
+        Try
+            SqlConnection.Open()
+
+            MessageBox.Show("Connection Successful")
+
+            SqlConnection.Close()
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            SqlConnection.Dispose()
+        End Try
+
+    End Sub
 
 End Class
