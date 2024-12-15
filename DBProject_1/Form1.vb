250c10c712f8edd0bc1d312d65be97f7fa0326bc
14thDream <dndelara@proton.me>
2024-12-16 02:05:58 +0800

    Adjusted query  in logging in to accomodate change in database structure.

--- DBProject_1/Form1.vb
+++ DBProject_1/Form1.vb
@@ -29,7 +29,7 @@ Public Class Form1
         Try
             SqlConnection.Open()
 
-            Dim Query = $"SELECT * FROM edata WHERE username = '{TextBox_Username.Text}' AND password = '{TextBox_Password.Text}';"
+            Dim Query = $"SELECT * FROM accounts WHERE username = '{TextBox_Username.Text}' AND password = '{TextBox_Password.Text}';"
             Dim Command As New MySqlCommand(Query, SqlConnection)
             Dim Reader = Command.ExecuteReader()
 
